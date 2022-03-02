65b643d742926a1ccb4cdcca36e6e1b96f256734
Elodie Boin Zanchi <eboinzanchi@protonmail.com>
2022-03-02 18:29:15 +0100

    install redux-devtools-extension

--- my-app/src/app/store.js
+++ my-app/src/app/store.js
@@ -1,4 +1,6 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit'
+
+// const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 export const store = configureStore({
   reducer: {
