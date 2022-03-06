0a4e4349366683d40135f6d6d1c929fd2a073606
Elodie Boin Zanchi <eboinzanchi@protonmail.com>
2022-03-06 12:44:36 +0100

    corr nav-items style

--- my-app/src/components/MainNav/MainNav.js
+++ my-app/src/components/MainNav/MainNav.js
@@ -3,19 +3,27 @@ import './MainNav.scss'
 import Logo from '../Logo/Logo'
 import NavButton from '../NavButton/NavButton'
 
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { logout } from '../../features/authentication'
+
 
 const MainNav = () => {
-  const {token} = useSelector((state) => state.auth)
-  console.log(token)
+  const { token } = useSelector((state) => state.auth)
+  const dispatch = useDispatch()
+  const handleLogout = () => {
+    dispatch(logout())
+  }
 
   return (
     <nav className="main-nav">
       <Logo />
-      {/* <NavButton path={`/profile`} name='{user.name.toUpperCase()}'/> */}
-      {token ? <NavButton path={`/`} name='Logout' connected='true'/>
+      {token ? (
+        <div className='nav-items'>
+          <NavButton path={`/profile`} name='Account'/>
+          <NavButton path={`/`} name='Logout' action={handleLogout}/>
+        </div>
+        )
       : <NavButton path={`/login`} name='Sign In'/> }
-      {/* <NavButton path={`/login`} name='Sign In'/> */}
     </nav>
   )
 }
