4150ada83f0e2f564b6fdb33ac697148fddd1e9c
Elodie Boin Zanchi <eboinzanchi@protonmail.com>
2022-03-20 12:01:43 +0100

    clean

--- my-app/src/components/NavButton/NavButton.js
+++ my-app/src/components/NavButton/NavButton.js
@@ -1,33 +1,26 @@
-import './NavButton.scss'
+import "./NavButton.scss"
 
-import { FaUserCircle, FaSignOutAlt } from 'react-icons/fa'
+import { FaUserCircle, FaSignOutAlt } from "react-icons/fa"
 
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom"
 
 const NavButton = ({ path, name, action }) => {
-
-  
-
   return (
-    <>
-    {action ?
     <li>
-      <Link to={path} className="nav-btn" onClick={action} >
+      {action ? (
+        <Link to={path} className="nav-btn" onClick={action}>
           {/* condition pour autres icônes */}
-          {name === 'Sign Out' ? <FaSignOutAlt/> : <FaUserCircle />}          
+          {name === "Sign Out" ? <FaSignOutAlt /> : <FaUserCircle />}
           <p>{name}</p>
-      </Link>
-    </li>
-    :
-      <li>
-        <Link to={path} className="nav-btn" >
-            {/* condition pour autres icônes */}
-            <FaUserCircle />
-            <p>{name}</p>
         </Link>
-      </li>
-    }
-    </>
+      ) : (
+        <Link to={path} className="nav-btn">
+          {/* condition pour autres icônes */}
+          <FaUserCircle />
+          <p>{name}</p>
+        </Link>
+      )}
+    </li>
   )
 }
 
